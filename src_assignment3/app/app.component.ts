eb4db5c656ae472a12927358c89ed1fd588bac36
MayurGorgal <34841199+MayurGorgal@users.noreply.github.com>
2019-04-15 22:58:24 +0530

    Add files via upload

---
+++ src_assignment3/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent 
+{
+  //ya assignment mdhe aaplyala marvellous infosystem as header dakhvaychy by updating app component
+  title = 'Marvellous Infosystems';
+}
